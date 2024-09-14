d9d451747e306eeb1da01a556668c266b3937d10
ItSushko14 <itsushkoandriy@gmail.com>
2024-09-14 16:35:04 +0300

    add random data to websocket

--- src/bank-whook/bank.gateway.ts
+++ src/bank-whook/bank.gateway.ts
@@ -4,6 +4,7 @@ import {
   SubscribeMessage,
   MessageBody,
 } from "@nestjs/websockets";
+import { log } from "console";
 import { Server } from "socket.io";
 
 @WebSocketGateway({
@@ -14,6 +15,27 @@ import { Server } from "socket.io";
 })
 export class BankGateway {
   @WebSocketServer() server: Server;
+
+  private interval: NodeJS.Timeout;
+
+  onModuleInit() {
+    this.interval = setInterval(() => {
+      const testData = {
+        trans_id: Date.now(),
+        trans_type: Math.random() > 0.5 ? "Зарахування" : "Списання",
+        trans_amount: (Math.random() * 1000).toFixed(2),
+        trans_date: new Date().toISOString(),
+      };
+      this.server.emit("bankWebHook", testData);
+      console.log("emit: ", testData);
+    }, 10000);
+  }
+
+  onModuleDestroy() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
 
   @SubscribeMessage("bankWebHook")
   handlePost(@MessageBody() data: any): void {
